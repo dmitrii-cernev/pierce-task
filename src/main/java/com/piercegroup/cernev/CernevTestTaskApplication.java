8c533417fa0f596ea4cbc1be481902754c18d9b1
Dmitrii Cernev <dmitrii.cernev@globallogic.com>
2024-03-04 10:22:09 +0100

    init commit

---
+++ src/main/java/com/piercegroup/cernev/CernevTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.piercegroup.cernev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CernevTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CernevTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piercegroup/cernev/CernevTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piercegroup.cernev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CernevTestTaskApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
